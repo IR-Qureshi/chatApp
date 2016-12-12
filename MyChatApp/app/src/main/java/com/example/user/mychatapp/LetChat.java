877ab146a0f0aa734abc5df2709e6eb98a445b48
IR_Qureshi <ibrehman1995@gmail.com>
2016-12-12 23:30:54 +0500

    setting up fireBase settings

---
+++ MyChatApp/app/src/main/java/com/example/user/mychatapp/LetChat.java
@@ -0,0 +1,13 @@
+package com.example.user.mychatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LetChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_let_chat);
+    }
+}
